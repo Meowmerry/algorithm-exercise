57027c94c3a75336319e79f66d1c16bbff607b62
MeowMerry <meow3525@gmail.com>
2022-08-07 16:11:36 -0500

    collectOddsAndEvens

--- collectOddsAndEvens.js
+++ collectOddsAndEvens.js
@@ -1,7 +1,9 @@
 /*
 
 collectOddsAndEvens
-Write a function called collectOddsAndEvens that accepts an array of positive integers and returns an object with the count of odd numbers and the count of even numbers. The keys in the object should be "odd" and "even".
+Write a function called collectOddsAndEvens that accepts an array of positive integers 
+and returns an object with the count of odd numbers and the count of even numbers. 
+The keys in the object should be "odd" and "even".
 Examples:
 collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 // { odd: 5, even: 4, });
@@ -27,3 +29,45 @@ collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
 // { odd: 8, even: 8 });
  */
+const collectOddsAndEvens = (array)=>{
+ 
+    /* ====== Solution 1 ======  */ 
+    // const cache = {'odd': 0 , 'even' : 0};
+    // for(let i = 0 ; i < array.length ; i+=1){
+    //     if(array[i] % 2 !== 0 ) cache['odd']+=1; 
+    //     if(array[i] % 2 === 0 ) cache['even']+=1; 
+    // }
+    // return cache;
+
+    /* ====== Solution 2 ======  */ 
+    return array.reduce((cache, curr)=>{
+        if(curr % 2 !== 0) cache['odd'] +=1;
+        if(curr % 2 === 0 ) cache['even']+=1; 
+
+        return cache;
+    },{'odd': 0 , 'even' : 0})
+}
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+// { odd: 5, even: 4, });
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// { odd: 5, even: 5, });
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]));
+// { odd: 6, even: 5,});
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));
+// { odd: 6, even: 6 });
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]));
+// { odd: 7, even: 6 });
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]));
+// { odd: 7, even: 7 });
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]));
+// { odd: 8, even: 7 });
+
+console.log(collectOddsAndEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]));
+// { odd: 8, even: 8 });

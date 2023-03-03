b904cbf6b9faafe399983ed9d617a38e6c36cae7
MeowMerry <meow3525@gmail.com>
2023-03-03 10:34:55 -0600

    minimumIsland

--- structy.net/Graph/islandCount.js
+++ structy.net/Graph/islandCount.js
@@ -7,7 +7,34 @@ An island is a vertically or horizontally connected region of land.
 */
 
 const islandCount = (grid) => {
+    const visited = new Set();
+    let count = 0;
+    for (let r = 0; r < grid.length; r += 1) {
+        for (let c = 0; c < grid[0].length; c += 1) {
+            // invoke helper function
+            if (exportIsland(grid, r, c, visited) === true) {
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
 
+const exportIsland = (grid, r, c, visited) => {
+    const rowInbounds = 0 <= r && r < grid.length;
+    const colInbounds = 0 <= c && c < grid.length;
+    if (!rowInbounds || !colInbounds) return false;
+
+    if (grid[r][c] === 'W') return false;
+
+    const position = r + ',' + c;
+    if (visited.has(position)) return false;
+    visited.add(position);
+    exportIsland(grid, r - 1, c, visited);
+    exportIsland(grid, r + 1, c, visited);
+    exportIsland(grid, r, c - 1, visited);
+    exportIsland(grid, r, c + 1, visited);
+    return true;
 }
 
 
